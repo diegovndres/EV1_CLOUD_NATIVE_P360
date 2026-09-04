1a9b0a93528bb4f0052a0962555d7706cab4fe44
kkrloszzz <ca.domke@duocuc.cl>
2026-09-04 09:39:36 -0400

    redireccion azure funcional

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  private msalService = inject(MsalService);
+
+  login(): void {
+    this.msalService.loginRedirect({
+      scopes: environment.apiConfig.scopes
+    });
+  }
+}
