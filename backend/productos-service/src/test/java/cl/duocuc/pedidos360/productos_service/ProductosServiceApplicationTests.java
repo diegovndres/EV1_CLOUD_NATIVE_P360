9fc28162a774e531e5ee0b0d1d97e144343cf4a3
diegovndres <diega.silvar@duocuc.cl>
2026-09-07 16:36:15 -0300

    microservicio producto

---
+++ backend/productos-service/src/test/java/cl/duocuc/pedidos360/productos_service/ProductosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.pedidos360.productos_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
