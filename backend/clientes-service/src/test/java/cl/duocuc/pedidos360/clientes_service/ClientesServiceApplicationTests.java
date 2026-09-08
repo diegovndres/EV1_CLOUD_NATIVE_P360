3c27862ded9da9736b0e12d4b30d6209035b5644
diegovndres <diega.silvar@duocuc.cl>
2026-09-07 21:49:45 -0300

    Microservicio Clientes Listo

---
+++ backend/clientes-service/src/test/java/cl/duocuc/pedidos360/clientes_service/ClientesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.pedidos360.clientes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
